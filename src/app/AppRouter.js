57276cab6eb4e4aa15a2ceda6f5e2259c5082886
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2018-10-19 22:21:14 +0200

    Removed Flow

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import routes from './routes/index';
 
 class AppRouter extends Component {
-  render() {
+  render = () => {
     return (
       <Switch>
         {routes.map((route, index) => (
@@ -11,7 +11,7 @@ class AppRouter extends Component {
         ))}
       </Switch>
     );
-  }
+  };
 }
 
 export default AppRouter;
